e9b97774f21109d94a395ad949455db3484711f6
mrperceval <marlow.perceval@gmail.com>
2024-02-23 17:16:55 +0000

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,23 +1,22 @@
 import "@/styles/globals.css";
+import { useState } from 'react'
 import Head from "next/head";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { CookieConsent, getCookieConsentValue } from "react-cookie-consent";
 
 export default function App({ Component, pageProps }) {
-  const handleAccept = () => {
+  const [accepted, setAccepted] = useState(false);
+  
+  const onAccept = () => {
+    setAccepted(true);
     if (window.dataLayer) {
       console.log("Accepted sent");
-      window.dataLayer.push({ event: "gtm.init_consen" });
+      window.dataLayer.push({ event: "gtm.init_consent" });
     } else {
       console.log("Accepted not sent");
     }
   };
-  const consent = () => {
-    if (getCookieConsentValue({ cookieName: "abgt600_cookie_consent" })) {
-      console.log("Consent given");
-      return true;
-    }
-  };
+
   return (
     <>
       <Head>
@@ -27,14 +26,14 @@ export default function App({ Component, pageProps }) {
       <Component {...pageProps} />
 
       <CookieConsent
-        onAccept={handleAccept}
+        onAccept={onAccept}
         buttonText="Accept"
         cookieName="abgt600_cookie_consent"
         expires={150}
       >
         This website used cookies to enhance the user experience.
       </CookieConsent>
-      {consent && <GoogleTagManager gtmId="GTM-PRQ9CM" />}
+      {accepted && <GoogleTagManager gtmId="GTM-PRQ9CM" />}
     </>
   );
 }
